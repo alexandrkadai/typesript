31a0dc60089a7f74b4221612f4630eb3aa473939
Alex Kadai <alexandrkadai@gmail.com>
2022-12-22 23:28:10 +0200

    21 videos changes

---
+++ app21.js
@@ -0,0 +1,7 @@
+"use strict";
+function fecthWithAuth(url, method) {
+    console.log(url);
+}
+fecthWithAuth('https://ups.com', 'post');
+let method = 'post';
+fecthWithAuth('https://ups.com', method);
--- app21.ts
+++ app21.ts
@@ -3,3 +3,7 @@ function fecthWithAuth(url: string, method: 'post' | 'get') {
 }
 
 fecthWithAuth('https://ups.com', 'post');
+
+let method = 'post';
+
+fecthWithAuth('https://ups.com', method as 'post');
